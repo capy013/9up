34b376a3d0f1a53d0c6fbeac310f3e855f85f97e
013 <lok13.work@gmail.com>
2026-07-30 17:42:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 9UP service worker (flat, root-level PNG icons). Bump CACHE_VERSION on change. */
-const CACHE_VERSION = 'v14-flat';
+const CACHE_VERSION = 'v15-flat';
 const CACHE_NAME = '9up-' + CACHE_VERSION;
 
 const CORE_ASSETS = [
